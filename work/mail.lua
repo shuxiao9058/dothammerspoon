aa9da36d9bf479ac59402208a0c5eaf1a856b969
Aaron Chi <shuxiao9058@gmail.com>
2022-08-10 22:33:45 +0800

    mail notify

---
+++ work/mail.lua
@@ -0,0 +1,141 @@
+local obj = {}
+obj._VERSION = '0.01'
+
+local mt = {__index = obj}
+
+local menuBarItem
+if not menuBarItem then menuBarItem = hs.menubar.new() end
+
+local unreadMailCount
+
+local threadId
+
+local mailQueryStr = 'folder:/Work/ and tag:work and tag:unread'
+
+local repeatTimer
+
+local function getRecentMailList()
+    local cmd = [[ notmuch search --format='json' --sort='newest-first' ]] ..
+                    "'" .. mailQueryStr .. ' and thread:{date:120mins..}' .. "'"
+    logger:d('cmd: ', cmd)
+    local output, status, type, rc = hs.execute(cmd, true)
+    if output then output = string.gsub(output, "[%s\n\r]+", "") end
+    if status and output then
+        logger:d('get recent unread mail info status:', status, ', output: ',
+                 output, ', type: ', type, ', rc: ', rc)
+        local mails = hs.json.decode(output)
+        if mails and #mails > 0 then return mails end
+    end
+end
+
+function textToImage(input, color)
+    if type(input) == 'number' then input = utf8.char(input) end
+    local styledInput = hs.styledtext.new(input, {
+        font = {
+            name = 'SF Pro',
+            size = 100
+        },
+        color = color
+    })
+    local canvas = hs.canvas.new({
+        x = 0,
+        y = 0,
+        h = 0,
+        w = 0
+    })
+    canvas:size(canvas:minimumTextSize(styledInput)):appendElements({
+        type = 'text',
+        text = styledInput
+    })
+    return canvas:imageFromCanvas()
+end
+
+local function notmuchNotify(newMailCount)
+    newMailCount = newMailCount or 0
+    -- if newUnreadMailCount and oldUnreadMailCount then
+    --     local newMailCount = newUnreadMailCount - oldUnreadMailCount
+    if newMailCount > 0 then
+        -- get recent mail or notify
+        local mails = getRecentMailList()
+
+        local maxThreadId = 0
+        local info = '\n'
+        if mails then
+            for i = 1, newMailCount do
+                local mail = mails[i]
+                if mail.thread then
+                    local mailThreadId = tonumber(mail.thread, 16)
+                    if not threadId or threadId < mailThreadId then
+                        maxThreadId = mailThreadId
+                    else
+                        logger:d('break forloop, threadId', threadId,
+                                 ', maxThreadId: ', maxThreadId,
+                                 ', mailThreadId: ', mailThreadId)
+                        break
+                    end
+                end
+
+                if i > 5 then break end
+
+                local mailInfo = tostring(i) .. '. ' .. (mail.subject or '') ..
+                                     ' - ' .. (mail.date_relative or '')
+                info = info .. '\n' .. mailInfo
+            end
+        end
+
+        if maxThreadId and maxThreadId > 0 then threadId = maxThreadId end
+
+        local notifyAttr = {
+            alwaysPresent = true,
+            setIdImage = textToImage(0x10020E),
+            title = 'Notmuch new mail notifications',
+            subTitle = string.format(
+                'Received %d new mails since last refresh.', newMailCount),
+            informativeText = info,
+            withdrawAfter = 0
+        }
+
+        local notify = hs.notify.new(nil, notifyAttr)
+        notify:alwaysPresent(true):autoWithdraw(false):send()
+    end
+end
+
+local function checkUnreadMail(eventName, params)
+    -- local search = 'tag:inbox'
+    -- local search = 'folder:Work/ and tag:inbox and tag:unread'
+    local search = mailQueryStr
+    local output, status, _, _ = hs.execute("notmuch count " .. search, true)
+    local focusedStyle = {
+        font = hs.styledtext.defaultFonts.menuBar
+        -- color = hs.drawing.color.hammerspoon.osx_green,
+    }
+
+    if output then output = string.gsub(output, "[%s\n\r]+", "") end
+    local newUnreadMailCount
+    if status and unreadMailCount ~= output then
+        newUnreadMailCount = output
+        local displayText = '📮 ' .. output
+        local title = hs.styledtext.new(displayText, focusedStyle)
+        menuBarItem:setTitle(title)
+    end
+
+    -- -- test
+    -- notmuchNotify(2)
+
+    -- new mail received
+    if newUnreadMailCount and oldUnreadMailCount then
+        local newMailCount = newUnreadMailCount - oldUnreadMailCount
+        notmuchNotify(newMailCount)
+    end
+
+    oldUnreadMailCount = newUnreadMailCount
+    -- logger:d('get mail count status:', status, ', output: ', output)
+end
+
+function obj:start()
+    checkUnreadMail()
+    -- repeatTimer = hs.timer.doEvery(5, checkUnreadMail)
+    hs.urlevent.bind('checkUnreadMail', checkUnreadMail)
+end
+
+return obj
